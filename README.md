946c0aa859e43d899a1b976b2363a581b15f721d
OpaqueGlass <opaqueglass@126.com>
2025-05-04 22:15:57 +0800

    init. repo

---
+++ README.md
@@ -0,0 +1,78 @@
+
+# A little MCP server for siyuan-note
+
+[中文](./README_zh_CN.md)
+
+## ✨ Quick Start
+
+- Download from the marketplace or 1. unzip the `package.zip` in Release, 2. move the folder to `workspace/data/plugins/`, 3. and rename the folder to `syplugin-aMCPServer`;
+- Enable the plugin;
+- The plugin listens on port `16806` by default (Host: `127.0.0.1`), please use `http://127.0.0.1:16806/sse` as the server access address;
+
+> ⭐ If this is helpful to you, please consider giving it a star!
+
+## 🔧 Supported Tools
+
+- [Search]
+  - Use keyword search;
+  - Use SQL search;
+- [Retrieve]
+  - Fetch document kramdown by ID;
+  - List notebooks;
+- [Write]
+  - Append content to diary;
+  - Append content to a specific document by ID;
+
+## ❓ Frequently Asked Questions
+
+- Q: How do I use it in an MCP client?
+  - A: Modify the MCP application's configuration to select the SSE type and configure the endpoint, for example:
+    ```json
+    {
+    "mcpServers": {
+        "siyuan": {
+            "type": "sse",
+            "command": "http://127.0.0.1:16806/sse",
+            "args": [
+                ""
+            ],
+            "env": {},
+            "auto_approve": false
+        }
+    }
+    }
+    ```
+- Q: My MCP client does not support HTTP-based communication, only stdio.
+  - Use an MCP Host application that supports HTTP communication;
+  - Or use the `node.js` + `mcp-remote` solution 
+    ```bash
+    npm install -g mcp-remote
+    ```
+    Use a configuration similar to the one below in the application:
+    ```json
+    {
+      "servers": [
+        {
+          "name": "Siyuan",
+          "key": "siyuan",
+          "description": "Read notes",
+          "command": "npx",
+          "args": [
+            "mcp-remote",
+            "http://127.0.0.1:16806/sse"
+          ],
+          "isActive": true
+        }
+      ]
+    }
+    ```
+- Q: What are some common MCP clients?
+  - Please refer to: https://github.com/punkpeye/awesome-mcp-clients or https://modelcontextprotocol.io/clients;
+
+## 🙏 References & Acknowledgements
+
+> Some dependencies are listed in `package.json`.
+
+| Developer/Project                                                         | Project Description           | Citation         |
+|---------------------------------------------------------------------|----------------|--------------|
+| [thuanpham582002/tabby-mcp-server](https://github.com/thuanpham582002/tabby-mcp-server) | Provides MCP service within the terminal software Tabby; MIT License | Implementation method of MCP service |
---
+++ webpack.config.js
@@ -0,0 +1,132 @@
+const path = require("path");
+const fs = require("fs");
+const webpack = require("webpack");
+const {EsbuildPlugin} = require("esbuild-loader");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const ZipPlugin = require("zip-webpack-plugin");
+
+// 使用change-dir命令更改dev的目录
+const devDistDirInfo = "./scripts/devInfo.json";
+const loadDirJsonContent = fs.existsSync(devDistDirInfo)
+  ? JSON.parse(fs.readFileSync(devDistDirInfo, "utf-8"))
+  : {};
+const devDistDir = loadDirJsonContent["devDir"] ?? "./dev";
+const distDir = devDistDir
+
+module.exports = (env, argv) => {
+    const isPro = argv.mode === "production";
+    const plugins = [
+        new MiniCssExtractPlugin({
+            filename: isPro ? "dist/index.css" : "index.css",
+        })
+    ];
+    let entry = {
+        "index": "./src/index.ts",
+    };
+    if (isPro) {
+        entry = {
+            "dist/index": "./src/index.ts",
+        };
+        plugins.push(new webpack.BannerPlugin({
+            banner: () => {
+                return fs.readFileSync("LICENSE").toString();
+            },
+        }));
+        plugins.push(new CopyPlugin({
+            patterns: [
+                {from: "preview.png", to: "./dist/"},
+                {from: "icon.png", to: "./dist/"},
+                {from: "README*.md", to: "./dist/"},
+                {from: "plugin.json", to: "./dist/"},
+                {from: "LICENSE", to: "./dist/"},
+                {from: "CHANGELOG.md", to: "./dist/"},
+                {from: "src/i18n/", to: "./dist/i18n/"},
+            ],
+        }));
+        plugins.push(new ZipPlugin({
+            filename: "package.zip",
+            algorithm: "gzip",
+            include: [/dist/],
+            pathMapper: (assetPath) => {
+                return assetPath.replace("dist/", "");
+            },
+        }));
+    } else {
+        plugins.push(new CopyPlugin({
+            patterns: [
+                {from: "src/i18n/", to: "./i18n/"},
+                {from: "preview.png", to: ".x"},
+                {from: "icon.png", to: "."},
+                {from: "README*.md", to: "."},
+                {from: "plugin.json", to: ""},
+                {from: "LICENSE", to: "."},
+            ],
+        }));
+    }
+    return {
+        mode: argv.mode || "development",
+        watch: !isPro,
+        devtool: isPro ? false : "eval",
+        target: "node",
+        output: {
+            filename: "[name].js",
+            path: isPro ? path.resolve(__dirname) : distDir,//path.resolve(__dirname, 'dist'),
+            libraryTarget: "commonjs2",
+            library: {
+                type: "commonjs2",
+            },
+        },
+        externals: {
+            siyuan: "siyuan",
+        },
+        entry,
+        optimization: {
+            minimize: true,
+            minimizer: [
+                new EsbuildPlugin(),
+            ],
+        },
+        resolve: {
+            extensions: [".ts", ".scss", ".js", ".json"],
+            alias: {
+                "@": path.resolve(__dirname, "src"),
+            },
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts(x?)$/,
+                    include: [path.resolve(__dirname, "src")],
+                    use: [
+                        {
+                            loader: "esbuild-loader",
+                            options: {
+                                target: "es6",
+                            }
+                        },
+                    ],
+                },
+                {
+                    test: /\.scss$/,
+                    include: [path.resolve(__dirname, "src")],
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        {
+                            loader: "css-loader", // translates CSS into CommonJS
+                        },
+                        {
+                            loader: "sass-loader", // compiles Sass to CSS
+                        },
+                    ],
+                },
+                {
+                    test: /\.md$/,
+                    include: [path.resolve(__dirname, "static")],
+                    use: 'raw-loader',
+                }
+            ],
+        },
+        plugins,
+    };
+};
